116a91cd1b7a3a69d070977967bdde2143fe35d4
serg <sergeusprecious@gmail.com>
2024-05-01 21:24:01 +0400

    fix models: books

--- src/models/books.py
+++ src/models/books.py
@@ -12,7 +12,9 @@ class ShortBook(BookIdMixin):
 class Book(ShortBook):
     year: datetime.datetime
     pages: Optional[int]
+    size: Optional[str]
     published_by: Optional[str] = None
     description: Optional[str] = None
+    cover: Optional[str] = None
     download_links: list[dict[str: str]]  # extension:link
-    # tor_link: dict[str: str] # extension:link
+    # tor_link: dict[str: str]  # extension:link
