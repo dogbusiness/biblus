8d889bb571d5bd795508dcf8b8838db9f436a055
serg <sergeusprecious@gmail.com>
2024-06-14 19:55:28 +0400

    fix:models, service;feat:api tests with mocked db

--- src/tests/mocks.py
+++ src/tests/mocks.py
@@ -1,10 +1,6 @@
 class MockedBooksDB:
-    
-    async def get_object_by_id(
-        self,
-        table: str,
-        id
-    ) -> dict | None:
+
+    async def get_object_by_id(self, table: str, id) -> dict | None:
         return {
             "book_id": "0ad94e7c-27d5-47ad-81d4-6b6edf764ad0",
             "author": "Fyodor Dostoevskiy",
@@ -15,8 +11,8 @@ class MockedBooksDB:
             "description": "Crime and Punishment follows the mental anguish and moral dilemmas of Rodion Raskolnikov, an impoverished ex-student in Saint Petersburg who plans to kill an unscrupulous pawnbroker, an old woman who stores money and valuable objects in her flat.",
             "cover": "https://en.wikipedia.org/wiki/Crime_and_Punishment#/media/File:Crimeandpunishmentcover.png",
             "download_links": [{".pdf": ["https://somelink.com/crime_punish.pdf"]}],
-            "tor_link": {".pdf": "http://someonionlink.onion/crime_punish.pdf"}
-}
+            "tor_link": {".pdf": "http://someonionlink.onion/crime_punish.pdf"},
+        }
 
     async def search_objects(  # noqa: E704
         self,
@@ -29,10 +25,10 @@ class MockedBooksDB:
     ) -> list[dict] | list[None]:
         return [
             {
-            "book_id": "0ad94e7c-27d5-47ad-81d4-6b6edf764ad0",
-            "title": "Crime and Punishment",
-            "author": "Fyodor Dostoevskiy",
-            "year": 1866,
-            "pages": 435
+                "book_id": "0ad94e7c-27d5-47ad-81d4-6b6edf764ad0",
+                "title": "Crime and Punishment",
+                "author": "Fyodor Dostoevskiy",
+                "year": 1866,
+                "pages": 435,
             }
         ]
--- src/tests/testdata.py
+++ src/tests/testdata.py
@@ -1,10 +1,10 @@
 BOOK_SEARCH_SUCCESS = [
     {
-    "book_id": "0ad94e7c-27d5-47ad-81d4-6b6edf764ad0",
-    "author": "Fyodor Dostoevskiy",
-    "title": "Crime and Punishment",
-    "year": 1866,
-    "pages": 435
+        "book_id": "0ad94e7c-27d5-47ad-81d4-6b6edf764ad0",
+        "author": "Fyodor Dostoevskiy",
+        "title": "Crime and Punishment",
+        "year": 1866,
+        "pages": 435,
     }
 ]
 
@@ -17,5 +17,5 @@ GET_BOOK_BY_ID_SUCCESS = {
     "description": "Crime and Punishment follows the mental anguish and moral dilemmas of Rodion Raskolnikov, an impoverished ex-student in Saint Petersburg who plans to kill an unscrupulous pawnbroker, an old woman who stores money and valuable objects in her flat.",
     "cover": "https://en.wikipedia.org/wiki/Crime_and_Punishment#/media/File:Crimeandpunishmentcover.png",
     "download_links": [{".pdf": "https://somelink.com"}],
-    "tor_link": {".pdf": "http://someonionlink.onion"}
+    "tor_link": {".pdf": "http://someonionlink.onion"},
 }
